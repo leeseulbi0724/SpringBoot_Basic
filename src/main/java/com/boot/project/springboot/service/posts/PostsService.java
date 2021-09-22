79c9154cf82c6eb474f25e5c56759ad6cfd69d88
leeseulbi0724 <tmfql4428@naver.com>
2021-09-22 22:25:05 +0900

    스프링부트 JPA 등록/수정/조회 API 생성

---
+++ src/main/java/com/boot/project/springboot/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.boot.project.springboot.service.posts;
+
+import com.boot.project.springboot.domain.posts.Posts;
+import com.boot.project.springboot.domain.posts.PostsRepository;
+import com.boot.project.springboot.web.dto.PostsResponseDto;
+import com.boot.project.springboot.web.dto.PostsSaveRequestDto;
+import com.boot.project.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update (Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findByid(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+}
