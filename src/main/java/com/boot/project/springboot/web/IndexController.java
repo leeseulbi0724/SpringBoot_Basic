282348a45739e5af0f3f9bb099554ed0e84ff4c5
leeseulbi0724 <tmfql4428@naver.com>
2021-09-23 12:38:33 +0900

    게시글 등록화면 생성

---
+++ src/main/java/com/boot/project/springboot/web/IndexController.java
@@ -0,0 +1,23 @@
+package com.boot.project.springboot.web;
+
+import com.boot.project.springboot.service.posts.PostsService;
+import com.boot.project.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
